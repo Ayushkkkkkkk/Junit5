501af9922b1f01f2f23f38d5c06c75df12f05a43
Ayush_sccg <ayush1240945@gmail.com>
2023-12-08 14:50:18 +0545

    close to zero

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,37 +7,64 @@ import static java.lang.Math.min;
 
 public class Main {
 
+//    public static void main(String[] args) {
+//        int arr[] = {1 , 2 , 3 , 4 , 5};
+//        closestToZero(arr);
+//    }
 
-    public static int minimumValue(int[] arr) {
-        int mini = 100000000;
-        for (int x : arr){
-            mini = min(mini , x);
+    public static int closestToZero(int[] arr) {
+        int fx = arr.length;
+        int[] temp = new int[fx];
+        for (int i = 0 ; i < fx ; i++){
+            temp[i] = arr[i];
         }
-        return mini;
-    }
 
-    public static int maximumValue(int[] arr) {
-        int maxi = -1000000;
-        for (int x : arr){
-            maxi = max(maxi , x);
+        ArrayList<Integer> list = new ArrayList<>();
+        int mini = 10000000;
+        for (int i = 0 ; i < arr.length ; i++){
+            int count = 0;
+            if(arr[i] < 0){
+                while(arr[i]!= 0){
+                    count++;
+                    arr[i] += 1;
+                }
+                mini = min(mini , count);
+                count = 0;
+            }
+            else{
+                while(arr[i]!=0){
+                    count++;
+                    arr[i] -= 1;
+                }
+                mini = min(mini , count);
+                count = 0;
+            }
         }
-        return maxi;
-    }
-
-    public static int NumberOfElements(int[] arr) {
-        return arr.length;
-    }
-
-    public static double averageValue(int[] arr) {
-        double avg = 0.0;
-        double sum = 0.0;
-
-        for (int i = 0 ; i < arr.length; i++){
-            sum += arr[i];
+//        System.out.println(arr);
+        for (int i = 0 ; i < arr.length ; i++){
+            if(temp[i] - mini == 0){
+                list.add(temp[i]);
+            }
+            if(temp[i] + mini == 0){
+                list.add(temp[i]);
+            }
         }
-        avg = sum / arr.length;
-        return avg;
+        for (int x : list){
+            System.out.println(x);
+        }
+
+        if(list.size() > 1){
+            int maxi = -1;
+            for (int x : list){
+                maxi = max(maxi , x);
+            }
+            return maxi;
+        }
+
+        return list.get(0);
     }
+
+
 
 
 }
--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -2,6 +2,8 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static java.lang.Math.max;
 import static java.lang.Math.min;
 import static org.junit.jupiter.api.Assertions.*;
@@ -10,49 +12,30 @@ class MainTest {
 
     @Test
     void checkMinimumValue() {
-        int[] arr = {6, 9, 15, -2, 92, 11};
+        int[] arr = {-1, 2, 3, 29, 11};
+        Arrays.sort(arr);
         int mini = 100000000;
         for (int x : arr) {
             mini = min(mini, x);
         }
-        int expected = mini;
-        int actual = Main.minimumValue(arr);
+        int expected = -1;
+        int actual = Main.closestToZero(arr);
         assertEquals(expected, actual);
     }
 
     @Test
-    void checkMaximumValue() {
-        int[] arr = {6, 9, 15, -2, 92, 11};
-        int maxi = -1000000;
+    void draw(){
+        int[] arr = {-1, 2, 3, 1, 29, 11};
+        Arrays.sort(arr);
+        int mini = 100000000;
         for (int x : arr) {
-            maxi = max(maxi, x);
+            mini = min(mini, x);
         }
-        int expected = maxi;
-        int actual = Main.maximumValue(arr);
+        int expected = 1;
+        int actual = Main.closestToZero(arr);
         assertEquals(expected, actual);
     }
 
-    @Test
-    void countNumberInSequence() {
-        int[] arr = {6, 9, 15, -2, 92, 11};
-        int expected = arr.length;
-        int actual = Main.NumberOfElements(arr);
-        assertEquals(expected, actual);
-    }
 
-    @Test
-    void checkAverageValue() {
-        int[] arr = {6, 9, 15, -2, 92, 11};
-        double avg = 0.0;
-        double sum = 0.0;
-
-        for (int i = 0; i < arr.length; i++) {
-            sum += arr[i];
-        }
-        avg = sum / arr.length;
-        double expected = avg;
-        double actual = Main.averageValue(arr);
-        assertEquals(expected, actual);
-    }
 
 }
